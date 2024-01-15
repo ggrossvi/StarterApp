bd7ec0bf13755e4228eef2c87eb439e06a5d05c2
Gloria Villa <hello.gloria@gmail.com>
2024-01-14 23:31:30 -0800

    added change background color functionality

--- StarterApp/ViewController.swift
+++ StarterApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor {
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        return UIColor(red:red, green: green, blue:blue, alpha: 0.5)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    
+    
 }
 
